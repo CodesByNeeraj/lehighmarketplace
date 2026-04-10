18a821cd4b91163db456d3eca5300b88c424e59e
CodesByNeeraj <neerajlaks@gmail.com>
2026-04-10 12:00:43 -0400

    added item id params

---
+++ server/routes/updateListing.js
@@ -0,0 +1,32 @@
+import authenticate from '../middleware/auth.js'
+import express from "express"
+import prisma from '../db/prisma.js';
+
+const router = express.Router();
+
+router.put('/update-listing/:item_id',authenticate,async(req,res)=>{
+    try{
+        const studentId = req.user.id
+        const item_id = parseInt(req.params.item_id)
+        const {title,description,price,condition,meetupLocation} = req.body
+        const listing = await prisma.listing.findUnique({where:{item_id}})
+        if (!listing){
+            return res.status(404).json({error:"Listing not found"})
+        }
+        if (listing.seller_id!==studentId){
+            return res.status(403).json({error:"Forbidden: Cannot update other seller's listings"})
+        }
+        await prisma.listing.update({
+            where:{ item_id },
+            data:{ title, description, price, condition, meetup_location: meetupLocation }
+        })
+    res.status(200).json({message:"Listing successfully updated!"})
+
+    }catch(err){
+        console.error(err)
+        res.status(500).json({error:"Internal server error!"})
+    }
+
+})
+
+export default router;
