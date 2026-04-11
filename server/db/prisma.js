2ca97bd2083d03253fc077c1379d974e3ffc19a9
CodesByNeeraj <neerajlaks@gmail.com>
2026-04-11 18:49:37 -0400

    feat:added waitlist table for pre launch

--- server/db/prisma.js
+++ server/db/prisma.js
@@ -1,11 +1,30 @@
 import 'dotenv/config';
 import pkg from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
+import { withFirewall } from 'prisma-firewall';
 
 const { PrismaClient } = pkg;
 
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
 
-//db connection
-export default prisma;
+const baseClient = new PrismaClient({ adapter });
+
+const prisma = withFirewall(baseClient, {
+  rules: {
+    blockDeleteWithoutWhere: true,
+    blockUpdateWithoutWhere: true,
+    blockDeleteSingleWithoutId: true,
+    blockUpdateSingleWithoutId: true,
+    blockFindManyWithoutLimit: true,
+    maxRows: 1000,
+    blockDangerousRawPatterns: true,
+    blockOperatorInjection: true,
+    rateLimit: {
+      operations: ['deleteMany', 'updateMany'],
+      maxPerMinute: 10
+    },
+    auditLog: true
+  }
+})
+
+export default prisma
