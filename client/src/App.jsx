80dfacef638178d6accce5aa52c54cf1e085141e
CodesByNeeraj <neerajlaks@gmail.com>
2026-04-11 22:35:59 -0400

    fix:removed imports not needed for landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/authContext';
-
-// student pages
 import Landing from './pages/student/landing.jsx';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* public routes */}
-          <Route path="/landing" element={<Landing />} />
-         
-          {/* Student routes */}
-
-          {/* Admin routes */}
-
-          {/* Redirects: if someone visit the base url, redirect them to /login*/}
-          <Route path="/" element={<Navigate to="/landing" replace />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/" element={<Navigate to="/landing" replace />} />
       </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    </BrowserRouter>
   );
 }
