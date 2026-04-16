db5309a962cf01a150437a80872b1d21116fbcff
CodesByNeeraj <neerajlaks@gmail.com>
2026-04-16 16:06:40 -0400

    update:added route to get seller's profile

--- server/routes/getProfile.js
+++ server/routes/getProfile.js
@@ -19,3 +19,20 @@ router.get('/get-profile',authenticate,async(req,res)=>{
         res.status(500).json({error:'Server error'})
     }
 })
+
+//get any seller's public profile by their id
+router.get('/view-profile/:studentId',authenticate,async(req,res)=>{
+    try{
+        const {studentId} = req.params
+        const profileDetails = await prisma.studentProfile.findUnique({where:{student_id:studentId}})
+        if (!profileDetails){
+            return res.status(404).json({error:"Profile not found"})
+        }
+        res.status(200).json(profileDetails)
+    }catch(err){
+        console.error(err)
+        res.status(500).json({error:'Server error'})
+    }
+})
+
+export default router
