505f34e9cd1f6aeebd09a1d792f3f958f5e2d295
CodesByNeeraj <neerajlaks@gmail.com>
2026-04-22 12:09:25 -0400

    update: now updating deleted status with timestamp

--- server/routes/deleteListing.js
+++ server/routes/deleteListing.js
@@ -15,11 +15,12 @@ router.delete('/delete-listing/:item_id',authenticate,async(req,res)=>{
         if (listing.seller_id!==studentId){
             return res.status(403).json({error:"Forbidden: Cannot delete other seller's listings"})
         }
-        await prisma.listing.delete({
-            where:{
-                item_id
-            }
-    })
+
+        await prisma.listing.update({
+            where:{item_id},
+            data:{is_deleted:true, deleted_at: new Date()}
+        })
+        
     res.status(200).json({message:"Listing successfully deleted!"})
 
     }catch(err){
